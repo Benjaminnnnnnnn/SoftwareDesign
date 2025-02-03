f76bec1b27c7f90a2eabb8698db065816b9cc05b
Benjamin Aylward <benaylward87@gmail.com>
2025-02-03 17:07:10 -0330

    moving to pi

--- frontend/src/app/game/board.ts
+++ frontend/src/app/game/board.ts
@@ -9,26 +9,40 @@ const directions = [
   { dq: -1, dr: 1 }, // Top-left
 ];
 
-function generateBoard(num: number): Map<string, Hex> {
-  const newBoard = new Map<string, Hex>();
-  let generatedHexes = 0;
-
-  let q = 0;
-  let r = 0;
-  let ring = 1;
-  let step = 1;
-
-  while (generatedHexes < num) {}
-
-  return newBoard;
-}
 class Board {
+
   masterMap: Map<string, Hex>;
+  size: number;
+
   constructor() {
-    this.masterMap = generateBoard(19);
+    this.size = 19;
+    this.masterMap = this.generateBoard(this.size);
   }
 
   public resetBoard() {
-    this.masterMap = generateBoard(19);
+    this.masterMap = this.generateBoard(this.size);
   }
+
+  private generateBoard(num: number): Map<string, Hex> {
+    const newBoard = new Map<string, Hex>();
+    let generatedHexes = 0;
+
+    let q = 0;
+    let r = 0;
+    let ring = 1;
+    let size = 0;
+    let step = 1;
+
+    while (generatedHexes < num) {
+      for (let i = 0; i < step; i++) {
+              q += directions[size].dq;
+              r += directions[size].dr;
+              newBoard.set(`${q}${r}`, new Hex);
+              generatedHexes++;
+
+              if (hexCount >= numHexes) break;
+            }
+    }
+
+    return newBoard;
 }
