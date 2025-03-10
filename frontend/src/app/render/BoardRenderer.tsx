3a571dbb4828502a3d97352a0c1beb160b332205
Isabella Maria Day <144396136+belladay@users.noreply.github.com>
2025-03-10 15:26:31 -0230

    changed colour and size of grid

--- frontend/src/app/render/BoardRenderer.tsx
+++ frontend/src/app/render/BoardRenderer.tsx
@@ -17,12 +17,12 @@ const HexGrid = () => {
 
     appRef.current.init({
       width: 800,
-      height: 600,
-      backgroundColor: 0x123345,
+      height: 650,
+      backgroundColor: 0x6d86ad,
       view: canvasRef.current,
     });
     const board = new Board(3);
-    const hexRadius = 40;
+    const hexRadius = 70;
     const hexHeight = Math.sqrt(3) * hexRadius;
     const hexWidth = 2 * hexRadius;
     const axialToPixel = (q: number, r: number) => {
@@ -33,7 +33,7 @@ const HexGrid = () => {
     const createHexagon = (x: number, y: number, radius: number, tile: any) => {
       const hexagon = new PIXI.Graphics();
       hexagon.lineStyle(2, 0x000000); // Border color
-      hexagon.beginFill(0xffffff); // Fill color
+      hexagon.beginFill(0x7db07b); // Fill color
       for (let i = 0; i < 6; i++) {
         const angle = (Math.PI / 3) * i;
         const px = x + radius * Math.cos(angle);
@@ -57,7 +57,7 @@ const HexGrid = () => {
     // Render the board
     board.tiles.forEach((tile: Hex) => {
       const { x, y } = axialToPixel(tile.coord.q, tile.coord.r);
-      const hexagon = createHexagon(x + 400, y + 300, hexRadius, tile); // Center the grid
+      const hexagon = createHexagon(x + 400, y + 325, hexRadius, tile); // Center the grid
       if (appRef.current) {
         appRef.current.stage.addChild(hexagon);
       }
