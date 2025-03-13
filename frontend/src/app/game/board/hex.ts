a5421485e492507b037ec3707d3e6dfc218651dc
Isabella Maria Day <144396136+belladay@users.noreply.github.com>
2025-03-13 17:54:21 -0230

    fixed spelling error

--- frontend/src/app/game/board/hex.ts
+++ frontend/src/app/game/board/hex.ts
@@ -4,13 +4,13 @@ import { HexCoordinate } from "../types";
 export default class Hex {
   id: string;
   coord: HexCoordinate;
-  peice?: Piece;
+  piece?: Piece;
   neighbors: Hex[];
 
   constructor(q: number, r: number) {
     this.coord = { q, r };
     this.id = `${q},${r}`;
-    this.peice = undefined;
+    this.piece = undefined;
     this.neighbors = [];
   }
 }
