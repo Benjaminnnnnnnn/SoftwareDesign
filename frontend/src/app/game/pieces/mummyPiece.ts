34645829960061f2898386ec4b24b85ffc14f51c
Benjamin Aylward <benaylward87@gmail.com>
2025-04-02 11:44:21 -0230

    mummy

--- frontend/src/app/game/pieces/mummyPiece.ts
+++ frontend/src/app/game/pieces/mummyPiece.ts
@@ -6,6 +6,7 @@ import { statMasterList } from "./statMasterList";
 // Ensure "export default" is used
 export default class MummyPiece extends Piece {
   // TODO : Finish Implementation
+  rev: boolean;
   constructor(
     allied: boolean,
     stats: Stats | undefined,
@@ -17,10 +18,26 @@ export default class MummyPiece extends Piece {
       ? stats
       : statMasterList[this.id];
     this.max_health = max_health;
-    this.current_health = max_health
+    this.current_health = max_health;
     this.ad = ad;
     this.range = range;
     this.speed = speed;
     this.item = item;
+    this.rev = true;
+  }
+
+  public override takeDamage(damage: number): void {
+    this.current_health -= damage;
+    if (this.tile_id) {
+      this.damageHistory.add(this.tile_id);
+    }
+    if (this.current_health <= 0) {
+      if (this.rev) {
+        this.current_health = this.max_health;
+        this.rev = false;
+      } else {
+        this.alive = false;
+      }
+    }
   }
 }
